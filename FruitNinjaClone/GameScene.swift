b60121e1fd1a07682ec16e87e2607687fa477a28
Muammer Akca <muammerakca10@gmail.com>
2022-09-30 19:46:16 +0300

    Added createLives method

--- FruitNinjaClone/GameScene.swift
+++ FruitNinjaClone/GameScene.swift
@@ -21,6 +21,9 @@ class GameScene: SKScene {
     var livesImages = [SKSpriteNode]()
     var lives = 3
     
+    var activeSliceBG : SKShapeNode!
+    var activeSliceFG : SKShapeNode!
+    
     override func didMove(to view: SKView) {
      
         let background = SKSpriteNode(imageNamed: "sliceBackground")
@@ -49,7 +52,14 @@ class GameScene: SKScene {
     }
     
     func createLives(){
-        
+        for i in 0...3 {
+            let spriteNode = SKSpriteNode(imageNamed: "sliceLife")
+            spriteNode.position = CGPoint(x: CGFloat(834 + (i*70)), y: 720)
+            addChild(spriteNode)
+            livesImages.append(spriteNode)
+            
+            
+        }
     }
     
     func createSlices(){
